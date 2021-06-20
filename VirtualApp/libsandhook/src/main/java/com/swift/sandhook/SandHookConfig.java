fc808669714c6640d12d8390e8f840d5a4298084
0xF <admin@die.lu>
2021-06-20 22:13:11 +0800

    Fix hooklib crash above Android 12

--- VirtualApp/libsandhook/src/main/java/com/swift/sandhook/SandHookConfig.java
+++ VirtualApp/libsandhook/src/main/java/com/swift/sandhook/SandHookConfig.java
@@ -6,8 +6,28 @@ import android.os.Build;
 import com.swift.sandhook.lib.BuildConfig;
 
 public class SandHookConfig {
+public static int getPreviewSDKInt() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                return Build.VERSION.PREVIEW_SDK_INT;
+            } catch (Throwable e) {
+                // ignore
+            }
+        }
+        return 0;
+    }
+    public static int SDK_INT = 0;
 
-    public volatile static int SDK_INT = Build.VERSION.SDK_INT;
+    static {
+        if(getPreviewSDKInt() > 0)
+        {
+            SDK_INT = Build.VERSION.SDK_INT + 1;
+        }
+        else
+        {
+            SDK_INT = Build.VERSION.SDK_INT;
+        }
+    }
     //Debug status of hook target process
     public volatile static boolean DEBUG = true;
     //Enable compile with jit
